38e2be8308798e87c3dfa1515fdf68200041b53d
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-23 23:08:53 +1100

    feat: cli init

---
+++ src/cmd.rs
@@ -0,0 +1,15 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct CLI {
+    #[command(subcommand)]
+    pub command: SubCommands,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+pub enum SubCommands {
+    Init {},
+    Search {},
+    GenerateSchemas {},
+}
