20b83ea1338d3c2a1e089a1af0f709ad1eebc165
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-01-31 20:54:05 +1100

    fix: imports for build rs

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use reqwest::blocking::ClientBuilder;
 /**
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -11,8 +12,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
-use std::{env, path::PathBuf}
-use reqwest::blocking::ClientBuilder;
+use std::{env, path::PathBuf};
 use typify::{TypeSpace, TypeSpaceSettings};
 
 fn main() {
